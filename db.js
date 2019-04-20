8f82095fd0ff58f598839379259d84b0cae733e6
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-04-20 22:14:34 +0100

    Test case for creation of a user with userID factored in, passed

---
+++ db.js
@@ -0,0 +1,4 @@
+let records = {
+    users: []
+}
+
--- pop_exercise.js
+++ pop_exercise.js
@@ -1,9 +1,12 @@
 function pop_exercise(name,email,password){
     
+    let count = 1;
     let userCreate = function(name,email,password){
         this.name = name;
         this.email = email;
         this.password = password;
+        this.id = count;
+        count++;
     }
 
     return new userCreate(name,email,password);
